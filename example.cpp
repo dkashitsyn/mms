a20a1b3f649ed39534a3eae01b43514e6b148731
Dmitry Kashitsyn <korvin@deeptown.org>
2014-08-12 12:33:52 +0700

    Fixes examplet to use string stream, adds useful FIELDS macro

--- example.cpp
+++ example.cpp
@@ -35,41 +35,52 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 
+template <class A>
+class traverse_wrapper {
+public:
+	traverse_wrapper(A& a) : a(a) {}
+	template<class F> traverse_wrapper<A>& operator ,(F& f) { a(f); return *this; }
+
+private:
+	A& a;
+};
+
+#define FIELDS(...) \
+	template<class A> void traverseFields(A a) const { \
+		traverse_wrapper<A> wrapper(a);  wrapper, __VA_ARGS__; }
+
 template<class P>
 struct My {
-    int i;
-    mms::string<P> str;
-    mms::map<P, mms::string<P>, int> map;
+	int i;
+	mms::string<P> str;
+	mms::map<P, mms::string<P>, int> map;
 
-    // Expose struct's fields to mms
-    template<class A> void traverseFields(A a) const { a(i)(str)(map); }
+	// Expose struct's fields to mms
+	FIELDS(i, str, map);
 };
 
 int main()
 {
-    // Populate the struct
-    My<mms::Standalone> my;
-    my.i = 22;
-    my.str = "a string";
-    my.map.insert(std::make_pair("ten", 10));
-    my.map.insert(std::make_pair("eleven", 11));
-    my.map.insert(std::make_pair("twelve", 12));
-    
-    // Serialize it
-    std::ofstream out("filename");
-    size_t pos = mms::write(out, my);
-    out.close();
-    
-    // mmap() serialized data
-    int fd = ::open("filename", O_RDONLY);
-    struct stat st;
-    fstat(fd, &st);
-    char* data = (char*) mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
-    const My<mms::Mmapped>* pmy = reinterpret_cast<const My<mms::Mmapped>*>(data + pos);
-    
-    // Use the data
-    std::cout << pmy->i << std::endl;
-    std::cout << pmy->str << std::endl;
-    std::cout << pmy->map.size() << std::endl;
-    std::cout << pmy->map["twelve"] << std::endl;
+	// Populate the struct
+	My<mms::Standalone> my;
+	my.i = 22;
+	my.str = "a string";
+	my.map.insert(std::make_pair("ten", 10));
+	my.map.insert(std::make_pair("eleven", 11));
+	my.map.insert(std::make_pair("twelve", 12));
+
+	// Serialize it
+	std::stringstream out;
+	size_t pos = mms::write(out, my);
+
+	// Serialized data
+	std::string str = out.str();
+	const char* data = str.data();
+	const My<mms::Mmapped>* pmy = reinterpret_cast<const My<mms::Mmapped>*>(data + pos);
+
+	// Use the data
+	std::cout << pmy->i << std::endl;
+	std::cout << pmy->str << std::endl;
+	std::cout << pmy->map.size() << std::endl;
+	std::cout << pmy->map["twelve"] << std::endl;
 }
